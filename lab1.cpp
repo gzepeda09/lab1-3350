9f81bd606d58a055b707060f2be41e8b747c3390
Geno  Zepeda <102138826+gzepeda09@users.noreply.github.com>
2023-01-24 09:21:18 -0800

    my first commit for lab-1

---
+++ lab1.cpp
@@ -0,0 +1,18 @@
+//NAME: Geno Zepeda
+//CLASS: CMPS 3350
+//INSTR: Dr. Griesel
+//FILE: lab1.cpp
+
+
+#include <iostream>
+
+
+int main(void) {
+
+    int a = rand() % 10 + 1;     
+    
+    std::cout << a << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "My change!" << std::endl;
+    return 0;
+}
